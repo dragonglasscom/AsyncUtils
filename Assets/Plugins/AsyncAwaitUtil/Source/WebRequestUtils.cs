94dc4332068f64831ec287b8863260648866dca1
Dmitrij Kovaliov <dmitrij1998@gmail.com>
2018-03-30 20:41:42 +0300

    Project Upload

---
+++ Assets/Plugins/AsyncAwaitUtil/Source/WebRequestUtils.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityAsyncAwaitUtil;
+using UnityEngine.Networking;
+
+namespace Plugins.AsyncAwaitUtil.Source
+{
+    public static class WebRequestUtils
+    {
+        private static async Task<HttpResponseMessage> unsafe_ProcessWebrequest(UnityWebRequest request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+		
+            //Prepare cancelation
+            using (cancellationToken.Register(() =>
+            {
+                //Unity API is not thread safe, thus request abort must happen on Unity Main thread
+                SyncContextUtil.UnitySynchronizationContext.Post(_ =>
+                {
+                    // ReSharper disable once AccessToDisposedClosure
+                    request.Abort();
+                }, null);
+            }))
+            {
+				
+                await request.SendWebRequest();
+				
+                //The web request will be completed early with "Aborted error",
+                //Will need to throw here, to indicate that the task was aborted
+                cancellationToken.ThrowIfCancellationRequested();
+
+				
+                //Throw if any other error occured
+                if (request.isNetworkError || request.isHttpError)
+                {
+                    throw new Exception(request.error);
+                }
+                
+                return new HttpResponseMessage((HttpStatusCode)request.responseCode, request.downloadHandler.data,
+                    request.GetResponseHeaders());
+            }
+		
+        }
+
+        public static async Task<HttpResponseMessage> Get(string uri, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (SyncContextUtil.UnitySynchronizationContext == SynchronizationContext.Current)
+            {
+                //We are on unity thread, all good
+                using (var request = UnityWebRequest.Get(uri))
+                {
+                    return await unsafe_ProcessWebrequest(request, cancellationToken);
+                }
+            }
+		
+            //We were called outside of the Unity main thread
+            var tcs = new TaskCompletionSource<HttpResponseMessage>();
+		
+            //Push the async method onto Unity's main thread
+            SyncContextUtil.UnitySynchronizationContext.Post(async _ =>
+            {
+                try
+                {
+                    using (var request = UnityWebRequest.Get(uri))
+                    {
+                        var task = unsafe_ProcessWebrequest(request, cancellationToken);
+                        var result =  await task;
+                        tcs.SetResult(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            }, null);
+
+            return await tcs.Task;
+        }
+        
+        public static async Task<HttpResponseMessage> Post(string uri,string postData ,CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (SyncContextUtil.UnitySynchronizationContext == SynchronizationContext.Current)
+            {
+                //We are on unity thread, all good
+                using (var request = UnityWebRequest.Post(uri, postData))
+                {
+                    return await unsafe_ProcessWebrequest(request, cancellationToken);
+                }
+            }
+		
+            //We were called outside of the Unity main thread
+            var tcs = new TaskCompletionSource<HttpResponseMessage>();
+		
+            //Push the async method onto Unity's main thread
+            SyncContextUtil.UnitySynchronizationContext.Post(async _ =>
+            {
+                try
+                {
+                    using (var request = UnityWebRequest.Post(uri, postData))
+                    {
+                        var task = unsafe_ProcessWebrequest(request, cancellationToken);
+                        var result =  await task;
+                        tcs.SetResult(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            }, null);
+
+            return await tcs.Task;
+        }
+    }
+
+    public class HttpResponseMessage
+    {
+        public byte[] Data { get; }
+        
+        public HttpStatusCode StatusCode { get; }
+        
+        public Dictionary<string, string> ResponseHeaders { get; }
+
+        public HttpResponseMessage(HttpStatusCode statusCode, byte[] data, 
+            Dictionary<string, string> responseHeaders = null)
+        {
+            StatusCode = statusCode;
+            Data = data;
+            ResponseHeaders = responseHeaders;
+        }
+        
+        public bool IsSuccessStatusCode
+        {
+            get { return ((int)StatusCode >= 200) && ((int)StatusCode <= 299); }
+        }
+
+        public Stream ToStream()
+        {
+            return new MemoryStream(Data);
+        }
+
+        public string ReadToEnd()
+        {
+            using (var str = ToStream())
+            {
+                using (var sr = new StreamReader(str))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+
+        public T DeserializeAsJson<T>()
+        {
+            return JsonConvert.DeserializeObject<T>(ReadToEnd());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AsyncUtils
 A set of utilities for Asynchronous programming in Unity.
+
+
+## Motivation
+Use clean, async style web requests with better exception handling instead of coroutine hell.
+Provide easier task composition and cancellation.
+
+## Thread safety
+Most of the entities in Unity API are not thread safe.
+Methods from this library are thread aware and will be pushed to Unity's Main thread's synchronization context for execution.
+Cancellation of requests is also handled in the safe manner.
+
+## Installation
+Navigate to Player Settings and make sure your Scripting Runtime Version is set to .NET 4.6 Equivalent.
+Copy  Assets/Plugins folder into your Project's Assets folder.
+
+## References
+The library is based on [this] (https://assetstore.unity.com/packages/tools/async-await-support-101056) asset.
+It was enhanced with support for true async style Http requests.
